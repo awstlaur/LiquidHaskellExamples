336f0fc3d6f9d47e02bb38844fe555db440e6b1f
Alexander St Laurent <stlaurent.alexander@gmail.com>
2016-12-05 19:02:42 -0500

    clean up REMProof

--- REMProof.hs
+++ REMProof.hs
@@ -10,38 +10,28 @@ import Language.Haskell.Liquid.ProofCombinators
 
 {-@ type NonZeroNat = {v:Nat | v /= 0} @-}
 
-{-@ remainder :: Nat -> NonZeroNat -> Nat @-}
+{-@ remainder :: x:Nat -> y:NonZeroNat -> Nat / [x, y] @-}
 remainder :: Int -> Int -> Int
-remainder x y | x == 0    = 0
-              | x > y     = remainder (x - y) y
+remainder x y | x > y     = remainder (x - y) y
               | otherwise = x
 
 {-@ reflect remainder @-}
 
-{-@ type RemDecreases = x:Nat -> y:NonZeroNat -> {remainder x y <= x} @-}
-
-{-@ remDecreases :: RemDecreases @-}
-remDecreases :: Int -> Int -> Proof
-remDecreases x y
-    | x == 0
-    = remainder 0 y
-    ==. 0
-    <.  0
-    *** QED
-    
+{-@ remNonincreasing :: x:Nat -> y:NonZeroNat -> {remainder x y <= x} @-}
+remNonincreasing :: Int -> Int -> Proof
+remNonincreasing x y
     | x > y
     = remainder x y
-    ==. remainder (x - y) y
-    <.  (x - y)             ∵ remDecreases (x - y) y
-    <.  x
+    <=. (x - y)         ∵ remNonincreasing (x - y) y
     *** QED
-    
+
     | otherwise
     = remainder x y
-    ==. x
-    <.  x
     *** QED
 
-{-@ predicate Divides x y = (rem x y == 0) @-}
-
-
+{-@ remDecreasing :: x:Nat -> y:{y:NonZeroNat | x > y} -> {remainder x y < x} @-}
+remDecreasing :: Int -> Int -> Proof
+remDecreasing x y
+    = remainder x y
+    <=.  (x - y)         ∵ remNonincreasing (x - y) y
+    *** QED
