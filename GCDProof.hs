5274020115240f7dd26c8005bba6da5ebfce77ea
Alexander St Laurent <stlaurent.alexander@gmail.com>
2016-12-06 00:15:40 -0500

    add stuff to GCDProof

--- GCDProof.hs
+++ GCDProof.hs
@@ -14,55 +14,103 @@ import Language.Haskell.Liquid.ProofCombinators
 
 -- | Definitions -------------------------------------------------------------
 
+{-@ type NonZeroNat = {v:Nat | v /= 0} @-}
+
 -- definitions of remainder and gcd' were modified from
 -- the definitions of mod and gcd (respectively) found at
 -- goto.ucsd.edu/~rjhala/liquid/haskell/blog/blog/2013/12/14/gcd.lhs/
 
-{-@ remainder :: a:Nat -> b:{v:Nat| 0 < v} -> {v:Nat | v < b} / [a, b] @-}
+{-@ remainder :: x:Nat -> y:NonZeroNat -> {v:Nat | v < y} / [x, y] @-}
 remainder :: Int -> Int -> Int
-remainder a b
-  | a < b = a
-  | otherwise = remainder (a - b) b
+remainder x y
+  | x < y = x
+  | otherwise = remainder (x - y) y 
 
 {-@ reflect remainder @-}
 
-{-@ gcd' :: a:Nat -> b:{v:Nat | v < a} -> Nat / [a,b] @-}
+{-@ gcd' :: x:Nat -> y:{v:Nat | v < x} -> NonZeroNat / [x, y] @-}
 gcd' :: Int -> Int -> Int
-gcd' a b
-    | b == 0    = a
-    | otherwise = gcd' b (remainder a b)
+gcd' x y
+    | y == 0    = x
+    | otherwise = gcd' y (remainder x y)
 
 {-@ reflect gcd' @-}
 
-{-@ gcd :: a:Nat -> b:Nat -> Nat / [a, b] @-}
+{-@ gcd :: x:Nat -> y:Nat -> Nat / [x, y] @-}
 gcd :: Int -> Int -> Int
-gcd a b
-    | a < b     = gcd' b a
-    | a > b     = gcd' a b
-    | otherwise = a
+gcd x y
+    | x < y     = gcd' y x
+    | x > y     = gcd' x y
+    | otherwise = x
 
 {-@ reflect gcd @-}
 
--- | Propositions ------------------------------------------------------------
+-- | Simple Propositions -------------------------------------------------------
 
-{-@ type GCDCongruence = i:Nat -> j:Nat -> {i == j => gcd i == gcd j} @-}
+{-@ type GCDCongruence =
+    x0:Nat -> y0:Nat -> x1:Nat -> y1:Nat -> 
+        {x0 == x1 && y0 == y1 => gcd x0 y0 == gcd x1 y1} 
+@-}
 
--- | TODO
-{-@ gcdCongruence :: {true} @-}
-gcdCongruence :: Int -> Int -> Proof
-gcdCongruence _ _ = trivial *** QED
+-- | LiquidHaskell can derive this for uninterpreted functions!
+{-@ gcdCongruence :: GCDCongruence @-}
+gcdCongruence :: Int -> Int -> Int -> Int -> Proof
+gcdCongruence _ _ _ _ = trivial *** QED
 
-{-@ type GCDIdempotent =  @-}
+{-@ type GCDIdempotent = x:Nat -> {gcd x x == x} @-}
 
-{-@ gcdIdempotent :: x:Nat -> {gcd x x == x} @-}
+-- | LiquidHaskell knows that this hits the `otherwise` case!
+{-@ gcdIdempotent :: GCDIdempotent @-}
 gcdIdempotent :: Int -> Proof
 gcdIdempotent x
     = gcd x x
     *** QED
     
-{-@ type GCDCommutative = x:Nat -> y:{Nat | true } -> {gcd x y == gcd y x} @-}
+{-@ type GCDCommutative = x:Nat -> y:Nat -> {gcd x y == gcd y x} @-}
 
--- | TODO
-{-@ gcdCommutative :: {true} @-}
+-- | Our symmetric definition makes this easy!
+{-@ gcdCommutative :: GCDCommutative @-}
 gcdCommutative :: Int -> Int -> Proof
-gcdCommutative x y = trivial *** QED
+gcdCommutative x y
+    =   gcd  x y
+    ==. gcd  y x
+    *** QED
+
+-- | More Interesting Propositions ---------------------------------------------
+
+{-@ predicate Divides Y X = remainder X Y == 0 @-}
+
+{-@ dividesImpliesGCD' ::
+    x:Nat -> y:{v:NonZeroNat | v < x} -> {Divides y x => gcd' x y == y}
+@-}
+dividesImpliesGCD' :: Int -> Int -> Proof
+dividesImpliesGCD' x y
+    =   gcd' x y
+    ==. gcd' y (remainder x y)
+    ==. gcd' y 0
+    ==. y
+    *** QED
+
+{-@ type DividesImpliesGCD =
+    x:Nat -> y:{v:NonZeroNat | v < x} -> {Divides y x => gcd x y == y}
+@-}
+
+{-@ dividesImpliesGCD :: DividesImpliesGCD @-}
+dividesImpliesGCD x y
+    =   gcd x y
+    ==. y           ? dividesImpliesGCD' x y
+    *** QED
+
+-- | TODO: Prove this.
+{-@ type GCDDivides = x:Nat -> y:Nat -> {Divides (gcd x y) x} @-}
+
+-- {-@ gcdDivides :: GCDDivides @-}
+-- gcdDivides :: Int -> Int -> Proof
+-- gcdDivides x y = trivial *** QED
+
+-- | TODO: Prove this.
+{-@ type GCDAssociative =
+    x:Nat -> y:Nat -> z:Nat -> {gcd x (gcd y z) == gcd (gcd x y) z}
+@-}
+
+
