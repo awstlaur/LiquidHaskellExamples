b6a3ed98cbcbed38347d3d99f5c7d9ded41d3cd8
Alexander St Laurent <stlaurent.alexander@gmail.com>
2016-12-08 13:46:52 -0500

    yay

--- MapIssue.hs
+++ MapIssue.hs
@@ -1,11 +1,13 @@
 -- | https://github.com/ucsd-progsys/liquidhaskell/issues/885
 
-{-@ LIQUID "--higherorder" @-}
+{-@ LIQUID "--higherorder"     @-}
+{-@ LIQUID "--totality"        @-}
+{-@ LIQUID "--exact-data-cons" @-}
+
 
 import Prelude hiding(map)
 
+{-@ reflect map @-}
 map :: (a -> b) -> [a] -> [b]
 map _ [] = []
 map f (x:xs) = f x : map f xs
-
-{-@ reflect map @-}
